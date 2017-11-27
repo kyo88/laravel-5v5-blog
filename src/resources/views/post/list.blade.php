41c0ca10b5cf29110ab23cd7a0c7cc695fc61555
Long Ta <root@localhost.localdomain>
2017-11-27 07:46:37 +0000

    test web hook

--- src/resources/views/post/list.blade.php
+++ src/resources/views/post/list.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <h3>List of posts</h3>
-                {{--<h3> Test webhook</h3>--}}
+                <h3> Test webhook</h3>
                 @foreach( $posts as $post)
                     <div class="panel panel-default">
                         <div class="panel-heading"><a href="{{ route('post.detail', $post->id)  }}"><strong>{{ $post->title }}</strong></a> </div>
